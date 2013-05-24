483cf6515ed81c161adfa2f0cc5f56c759a99e6e
segui.joaquin@gmail.com <segui.joaquin@gmail.com@3dce3461-f52d-bb92-91f1-17c018cfeeb7>
2013-05-24 23:46:47 +0000

    Se simplificó el problema, proponiendo un Tablero, una Partida y elementos que se ponen y se sacan solamente. Falta implementar una matriz en la clase Tablero que representaria el tablero de juego para poder hacer los tests

--- trunk/BatallaNavalgo/src/model/Tablero.java
+++ trunk/BatallaNavalgo/src/model/Tablero.java
@@ -1,5 +1,24 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Tablero {
+	
+	
+	private ArrayList<ElementoDelJuego>[][] casilleros;
+	
+	public Tablero(){
+		
+		//this.casilleros = new ArrayList<ElementoDeJuego>()[10][10];
+		
+	}
+	
+	public void agregarElemento(ElementoDelJuego elemento, Posicion posicion) {
+		
+	}
+	
+	public void quitarElemento(Posicion posicion) {
+		
+	}
 
 }
