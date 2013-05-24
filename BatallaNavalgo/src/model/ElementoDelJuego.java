43af105ee0b2517517fa6382311fad36689121a9
segui.joaquin@gmail.com <segui.joaquin@gmail.com@3dce3461-f52d-bb92-91f1-17c018cfeeb7>
2013-05-24 23:46:47 +0000

    Se simplificó el problema, proponiendo un Tablero, una Partida y elementos que se ponen y se sacan solamente. Falta implementar una matriz en la clase Tablero que representaria el tablero de juego para poder hacer los tests

---
+++ BatallaNavalgo/src/model/ElementoDelJuego.java
@@ -0,0 +1,16 @@
+package model;
+
+public class ElementoDelJuego {
+	
+	
+	private Posicion posicion;
+
+	public Posicion getPosicion() {
+		return posicion;
+	}
+
+	public void setPosicion(Posicion posicion) {
+		this.posicion = posicion;
+	}
+
+}
---
+++ BatallaNavalgo/src/model/Posicion.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Posicion {
+	
+	private int fila;
+	private int columna;
+	
+	
+	public int getFila() {
+		return fila;
+	}
+	public void setFila(int fila) {
+		this.fila = fila;
+	}
+	public int getColumna() {
+		return columna;
+	}
+	public void setColumna(int columna) {
+		this.columna = columna;
+	}
+
+}
