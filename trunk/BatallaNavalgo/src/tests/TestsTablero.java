a70d5a498eb14e7ad62eeb58527f9978d35c9515
segui.joaquin@gmail.com <segui.joaquin@gmail.com@3dce3461-f52d-bb92-91f1-17c018cfeeb7>
2013-05-28 22:27:21 +0000

    Estoy tratando de implementar el método para quitar los elementos del tablero cuando se le pasa una posicion determinada, pero estoy teniendo problemas al probarlo. En el test estan detallados los pasos que se hacen, pero al correrlo me tira "Error de concurrencia".

--- trunk/BatallaNavalgo/src/tests/TestsTablero.java
+++ trunk/BatallaNavalgo/src/tests/TestsTablero.java
@@ -1,6 +1,9 @@
 package tests;
 
 import junit.framework.TestCase;
+import model.ElementoDelJuego;
+import model.ErrorFueraDeRango;
+import model.Posicion;
 import model.Tablero;
 
 import org.junit.Test;
@@ -13,8 +16,56 @@ public class TestsTablero extends TestCase{
 		Tablero miTablero = new Tablero();
 		
 		assertTrue(miTablero.getCantidadDeElementos() == 0);
-		
 	}
+	
+	@Test
+	public void testAlAgregarDiezElementosAlTableroLaCantidadDeElementosDebeSerIgualADiez() {
+		
+		Tablero miTablero = new Tablero();
+		ElementoDelJuego elemento = new ElementoDelJuego();
+		
+		for (int i=0; i<10; i++) {miTablero.agregarElemento(elemento);}
+		
+		assertTrue(miTablero.getCantidadDeElementos() == 10);
+	}
+	
+	@Test
+	public void testQuitarElemento() throws ErrorFueraDeRango {
+		
+		Tablero miTablero = new Tablero();
+		Posicion posicion1 = new Posicion(3,5);
+		ElementoDelJuego elemento = new ElementoDelJuego(posicion1);
+		
+		//Agrego un elemento en posicion1 y cuento
+		miTablero.agregarElemento(elemento);
+		assertTrue(miTablero.getCantidadDeElementos() == 1);
+		
+		//Agrego otro elemento con distinta posicion y cuento
+		Posicion posicion2 = new Posicion(3,8);
+		ElementoDelJuego otroelemento = new ElementoDelJuego(posicion2);
+		miTablero.agregarElemento(otroelemento);
+		assertTrue(miTablero.getCantidadDeElementos() == 2);
+		
+		//Quito todos los elementos en la posicion1 y cuento
+		miTablero.quitarElemento(posicion1);
+		assertTrue(miTablero.getCantidadDeElementos() == 1);
+/*
+		//Quito todos los elementos en posicion2 y cuento
+		miTablero.quitarElemento(posicion2);
+		assertTrue(miTablero.getCantidadDeElementos() == 0);
+*/
+/*
+		//Agrego dos elementos con posicion1 y cuento
+		miTablero.agregarElemento(elemento);
+		miTablero.agregarElemento(elemento);
+		assertTrue(miTablero.getCantidadDeElementos() == 2);
+*/	
+/*		
+		//Quito todos los elementos en la posicion1 y cuento
+		miTablero.quitarElemento(posicion1);
+		assertTrue(miTablero.getCantidadDeElementos() == 0);
+*/
+		}
 	
 
 }
