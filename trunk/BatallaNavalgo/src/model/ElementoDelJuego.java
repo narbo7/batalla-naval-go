baa6837cffc29f76d696b743cc6a4ce4a7c85cea
njivgy7@gmail.com <njivgy7@gmail.com@3dce3461-f52d-bb92-91f1-17c018cfeeb7>
2013-06-05 18:08:52 +0000

    ElementoDelJuego se cambia a abstracta; se agrega metodo abstracto pasarTurno.

--- trunk/BatallaNavalgo/src/model/ElementoDelJuego.java
+++ trunk/BatallaNavalgo/src/model/ElementoDelJuego.java
@@ -1,21 +1,21 @@
 package model;
 
 
-public class ElementoDelJuego {
-	
-	
+public abstract class ElementoDelJuego {
+
+
 	private Posicion posicion;
-	
+
 	public ElementoDelJuego(){
-		
+
 		this.posicion = new Posicion();
 	}
-	
+
 	public ElementoDelJuego(Posicion unaPosicion){
-		
+
 		this.posicion = unaPosicion;
 	}
-	
+
 	public Posicion getPosicion() {
 		return posicion;
 	}
@@ -23,5 +23,7 @@ public class ElementoDelJuego {
 	public void setPosicion(Posicion posicion) {
 		this.posicion = posicion;
 	}
+
+    public abstract void pasarTurno();
 
 }
