3cd24d8a8b9566f5f82b2e3c8349e5f57178ab86
sol.orive@gmail.com <sol.orive@gmail.com@3dce3461-f52d-bb92-91f1-17c018cfeeb7>
2013-06-19 00:36:58 +0000

    Descuento de puntos por disparo de bombas.

--- BatallaNavalgo/src/model/Jugador.java
+++ BatallaNavalgo/src/model/Jugador.java
@@ -1,47 +1,35 @@
 package model;
 
-import java.util.LinkedList;
-
 public class Jugador {
 
 	private int puntaje;
 
-	private LinkedList<Bomba> bombas;
 	private Bomba bombaSeleccionada;
 
 	public Jugador(){
         this.puntaje = 10000;
-//		this.bombas = new LinkedList<Bomba>();
-//		Posicion unaPosicion = new Posicion();
-//		Bomba unaBomba = new Disparo(unaPosicion);
-//		this.bombas.add(1,unaBomba);
-//		unaBomba = new MinaConRetardo(unaPosicion);
-//		this.bombas.add(2,unaBomba);
-//		unaBomba = new MinaDobleConRetardo(unaPosicion);
-//		this.bombas.add(3,unaBomba);
-//		unaBomba = new MinaTripleConRetardo(unaPosicion);
-//		this.bombas.add(4,unaBomba);
-//		unaBomba = new MinaPorContacto(unaPosicion);
-//		this.bombas.add(5,unaBomba);
 	}
 
 	public Bomba dispararBomba() {
-		// el jugador debe disparar una bomba seleccionada.
-		//setea la bomba en el tablero.
-		//TODO: Modificar esto, la vista va a indicar cual es la bomba seleccionada.
-		Bomba unaBomba = new MinaPorContacto();
-		this.seleccionarBomba(unaBomba);
+		this.descontarPuntaje(this.bombaSeleccionada.getCosto());
 		return bombaSeleccionada;
 	}
 	public void seleccionarBomba(Bomba unaBomba){
+		System.out.println("La bomba seleccionada es ...");
 		this.bombaSeleccionada= unaBomba;
+		System.out.println(unaBomba.toString());
 	}
 	public int getPuntaje() {
 		return puntaje;
 	}
 
-	public void setPuntaje(int unPuntaje) {
+	private void setPuntaje(int unPuntaje) {
 		this.puntaje = unPuntaje;
+	}
+	private void descontarPuntaje(int costo){
+		int nuevoPuntaje = getPuntaje();   
+		nuevoPuntaje -= costo;
+		setPuntaje(nuevoPuntaje);
 	}
 
 }
